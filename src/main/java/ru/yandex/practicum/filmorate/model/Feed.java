a6486a10cf99ea3fe69888a4dc48755ed19daa34
FedoRScorpioN <zapevalov@yandex.ru>
2022-12-26 14:09:54 +0500

    Feat: add-feed

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Feed.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.storage.feed.EventType;
+import ru.yandex.practicum.filmorate.storage.feed.Operation;
+
+@Data
+@Builder
+public class Feed {
+    private long eventId;
+    private int entityId;
+    private int userId;
+    private long timestamp;
+    private EventType eventType;
+    private Operation operation;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Feed;
+import ru.yandex.practicum.filmorate.storage.feed.EventType;
+import ru.yandex.practicum.filmorate.storage.feed.FeedStorage;
+import ru.yandex.practicum.filmorate.storage.feed.Operation;
+
+
+import java.time.Instant;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+
+    private final FeedStorage feedStorage;
+
+    public void add(int entityId, int userId,  EventType eventType, Operation operation) {
+        long timestamp = Instant.now().toEpochMilli();
+        feedStorage.addFeed(entityId, userId, timestamp, eventType, operation);
+    }
+
+    public List<Feed> getByUserId(int id) {
+        return feedStorage.findByUserId(id);
+    }
+}
